e441595d4ef5d1171d691891ac9744d6a6d29d5c
Seregorn <o_trauner@outlook.com>
2025-01-20 10:27:42 +0100

    Begin Desition tree (no Diagram yet)

--- Mushroom_Project/run-Mushroom.R
+++ Mushroom_Project/run-Mushroom.R
@@ -1,26 +1,37 @@
-
-# Setzen des Pfades und Einlesen der Daten
-
-#setwd("C:/Users/o_Tra/OneDrive/Studium/Assistenzsysteme/Mushroom_Project/Mushroom_Project/")
-
-Daten <- read.csv("./agaricus-lepiota.csv",header=TRUE,sep=";",fill=TRUE,stringsAsFactors=TRUE)
-
-#colnames(Daten)
-Daten[] <- lapply(Daten, as.factor)
-
-
-
-# Berechnung der linearen Regression
-
-#model <- lm( edible ~ cap_shape + cap_surface + cap_color, data=Daten)
-
-# Starten der Shiny-App
-
+# Librarys
 library(shiny)
 library(ggplot2)
 library(dplyr)
 library(shinyWidgets)
+library(caret)  # Für einfache Datenaufteilung
+library(rpart)
+library(rpart.plot)  # Für die Visualisierung des Baums
 
+
+# Einlesen der Daten
+Daten <- read.csv("./agaricus-lepiota.csv",header=TRUE,sep=";",fill=TRUE,stringsAsFactors=TRUE)
+Daten[] <- lapply(Daten, as.factor)
+
+# Aufteilen in Trainings- und Testdaten (80% Training, 20% Test)
+train_index <- createDataPartition(Daten$edible, p = 0.8, list = FALSE)
+train_data <- Daten[train_index, ]
+test_data <- Daten[-train_index, ]
+
+# Entscheidungsbaum erstellen
+set.seed(42)  # Reproduzierbarkeit sicherstellen
+decision_tree <- rpart(edible ~ ., data = train_data, method = "class")
+rpart.plot(decision_tree, type = 4, extra = 104)
+
+# Vorhersage auf Testdaten
+predictions <- predict(decision_tree, test_data, type = "class")
+
+# Konfusionsmatrix erstellen
+confusion_matrix <- confusionMatrix(predictions, test_data$edible)
+
+# Ergebnisse anzeigen
+print(confusion_matrix)
+
+# Starten der Shiny-App
 options(shiny.port = 8100)
 runApp("App-Mushroom")
 
